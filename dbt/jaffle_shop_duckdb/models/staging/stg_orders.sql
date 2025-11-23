601b187160f9fa483fa2865316d29c494a226e59
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2025-11-23 10:38:56 +0500

    Update stg_orders.sql

--- dbt/jaffle_shop_duckdb/models/staging/stg_orders.sql
+++ dbt/jaffle_shop_duckdb/models/staging/stg_orders.sql
@@ -1,9 +1,6 @@
 with source as (
 
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
+    {# Normally we would select from the table here, but we are using seeds to load our data in this project #}
     select * from {{ ref('raw_orders') }}
 
 ),
